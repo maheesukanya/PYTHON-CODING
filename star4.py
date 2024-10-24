43dfeff6f3a71b3d4520fdd66bf895ffceb69422
Sukanyasai Chebrolu <96374615+maheesukanya@users.noreply.github.com>
2024-10-24 22:23:32 +0530

    Add files via upload

---
+++ star4.py
@@ -0,0 +1,22 @@
+r=int(input())
+c=int(input())
+for i in range(r):
+    for j in range(r-1-i):
+        print(" ",end="")
+    for k in range(c):
+        print("*",end="")
+    print()
+    
+    
+    
+"""r=int(input())
+c=int(input())
+for i in range(r):
+    for j in range(i):
+        print(" ",end="")
+    for k in range(c):
+        print("*",end="")
+    print()"""
+    
+    
+    
