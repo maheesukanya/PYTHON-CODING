85c1f1eaf5938aab8c4c84e198794e10d00368a7
Sukanyasai Chebrolu <96374615+maheesukanya@users.noreply.github.com>
2024-10-24 22:22:08 +0530

    Add files via upload

---
+++ star1.py
@@ -0,0 +1,21 @@
+"""n=int(input())
+for i in range(n):
+    for j in range(n-2):
+        print("*",end="")
+        if j!=n-3:
+            print("_",end="")
+    print()"""
+        
+        
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+        if j!=n-1:
+            print("_",end="")
+    print()
+        
+        
+        
+        
+        
